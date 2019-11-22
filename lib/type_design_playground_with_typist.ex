9080e41e60dc3272937a7be22a8109bd266cb560
Nicholas Henry <nicholas.henry@gmail.com>
2019-11-22 09:44:47 -0500

    Generate application

---
+++ lib/type_design_playground_with_typist.ex
@@ -0,0 +1,18 @@
+defmodule TypeDesignPlaygroundWithTypist do
+  @moduledoc """
+  Documentation for TypeDesignPlaygroundWithTypist.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeDesignPlaygroundWithTypist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_design_playground_with_typist_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeDesignPlaygroundWithTypistTest do
+  use ExUnit.Case
+  doctest TypeDesignPlaygroundWithTypist
+
+  test "greets the world" do
+    assert TypeDesignPlaygroundWithTypist.hello() == :world
+  end
+end
